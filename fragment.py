20c775b8bcf386af571283ec088366430f41bfcf
Tim Popham <popham@uw.edu>
2014-11-18 09:26:59 -0800

    Random test file

---
+++ fragment.py
@@ -0,0 +1,18 @@
+#⊕
+#⊗
+( a  ⊗       b ) +   z
+x = a + d   ⊕ v -t⊕inj(m  ⊗ t) ⊗  a ⊕  n
+x = a + d   ⊕ v -t⊕inj(m  ⊗ t) ⊗  \
+  a ⊕  n
+x = (a + d   ⊕ v -t⊕inj(m  ⊗ t) ⊗  
+  a ⊕  n)
+x = a + d   ⊕ v -t⊕inj(m  ⊗ t) \
+  ⊗  a ⊕  n
+
+x = a.___oplus___(b)
+x = (a + b).___oplus___(b)
+
+# Some comments
+
+x = a.___oplus___(b + c)
+x = a + d   ⊕ (v -t⊕inj(m  ⊗ t)) ⊗  a ⊕  n
